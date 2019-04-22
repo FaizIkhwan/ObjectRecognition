dbbf288a3e6464ebef01892328498fce1831ed87
Faiz Kwan <faizikhwantop@gmail.com>
2019-04-22 23:36:18 +0800

    init commit

---
+++ Object recognition/Object recognition/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Object_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgTrackedObject = new System.Windows.Forms.DataGridView();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.confidenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.widthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.heightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yoloTrackingItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lstData = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cmbModels = new System.Windows.Forms.ComboBox();
+            this.cmbPretained = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnLoadPic = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.prgStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblStatus = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTrackedObject)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloTrackingItemBindingSource)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picResult
+            // 
+            this.picResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.picResult.Location = new System.Drawing.Point(30, 40);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(469, 469);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picResult.TabIndex = 0;
+            this.picResult.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Result";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(531, 40);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(884, 469);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgTrackedObject);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(876, 443);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tracked Object";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgTrackedObject
+            // 
+            this.dgTrackedObject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgTrackedObject.AutoGenerateColumns = false;
+            this.dgTrackedObject.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTrackedObject.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.typeDataGridViewTextBoxColumn,
+            this.confidenceDataGridViewTextBoxColumn,
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn,
+            this.widthDataGridViewTextBoxColumn,
+            this.heightDataGridViewTextBoxColumn});
+            this.dgTrackedObject.DataSource = this.yoloTrackingItemBindingSource;
+            this.dgTrackedObject.Location = new System.Drawing.Point(0, 4);
+            this.dgTrackedObject.Name = "dgTrackedObject";
+            this.dgTrackedObject.Size = new System.Drawing.Size(870, 433);
+            this.dgTrackedObject.TabIndex = 0;
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // confidenceDataGridViewTextBoxColumn
+            // 
+            this.confidenceDataGridViewTextBoxColumn.DataPropertyName = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.HeaderText = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.Name = "confidenceDataGridViewTextBoxColumn";
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            // 
+            // widthDataGridViewTextBoxColumn
+            // 
+            this.widthDataGridViewTextBoxColumn.DataPropertyName = "Width";
+            this.widthDataGridViewTextBoxColumn.HeaderText = "Width";
+            this.widthDataGridViewTextBoxColumn.Name = "widthDataGridViewTextBoxColumn";
+            // 
+            // heightDataGridViewTextBoxColumn
+            // 
+            this.heightDataGridViewTextBoxColumn.DataPropertyName = "Height";
+            this.heightDataGridViewTextBoxColumn.HeaderText = "Height";
+            this.heightDataGridViewTextBoxColumn.Name = "heightDataGridViewTextBoxColumn";
+            // 
+            // yoloTrackingItemBindingSource
+            // 
+            this.yoloTrackingItemBindingSource.DataSource = typeof(Alturos.Yolo.Model.YoloItem);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lstData);
+            this.tabPage2.Controls.Add(this.btnAdd);
+            this.tabPage2.Controls.Add(this.cmbModels);
+            this.tabPage2.Controls.Add(this.cmbPretained);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(876, 443);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Models";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lstData
+            // 
+            this.lstData.FormattingEnabled = true;
+            this.lstData.Location = new System.Drawing.Point(23, 60);
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(830, 329);
+            this.lstData.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(778, 17);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cmbModels
+            // 
+            this.cmbModels.FormattingEnabled = true;
+            this.cmbModels.Location = new System.Drawing.Point(434, 14);
+            this.cmbModels.Name = "cmbModels";
+            this.cmbModels.Size = new System.Drawing.Size(121, 21);
+            this.cmbModels.TabIndex = 3;
+            // 
+            // cmbPretained
+            // 
+            this.cmbPretained.FormattingEnabled = true;
+            this.cmbPretained.Location = new System.Drawing.Point(129, 16);
+            this.cmbPretained.Name = "cmbPretained";
+            this.cmbPretained.Size = new System.Drawing.Size(121, 21);
+            this.cmbPretained.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(327, 17);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "Select Model:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Select Pretrained:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnExecute);
+            this.groupBox1.Controls.Add(this.btnLoadPic);
+            this.groupBox1.Location = new System.Drawing.Point(30, 527);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1381, 85);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(231, 39);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(124, 39);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnLoadPic
+            // 
+            this.btnLoadPic.Location = new System.Drawing.Point(18, 39);
+            this.btnLoadPic.Name = "btnLoadPic";
+            this.btnLoadPic.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPic.TabIndex = 0;
+            this.btnLoadPic.Text = "Load Picture";
+            this.btnLoadPic.UseVisualStyleBackColor = true;
+            this.btnLoadPic.Click += new System.EventHandler(this.btnLoadPic_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prgStatus,
+            this.lblStatus});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 599);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1454, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // prgStatus
+            // 
+            this.prgStatus.Name = "prgStatus";
+            this.prgStatus.Size = new System.Drawing.Size(100, 22);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 22);
+            this.lblStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1454, 624);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgTrackedObject)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloTrackingItemBindingSource)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dgTrackedObject;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn confidenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn widthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn heightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource yoloTrackingItemBindingSource;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnLoadPic;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripProgressBar prgStatus;
+        private System.Windows.Forms.ToolStripLabel lblStatus;
+        private System.Windows.Forms.ComboBox cmbPretained;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cmbModels;
+        private System.Windows.Forms.ListBox lstData;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Object recognition/Object recognition/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alturos.Yolo;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Diagnostics;
+
+namespace Object_recognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            populateCmb(cmbModels, "*.names");
+            populateCmb(cmbPretained, "*.cfg");
+        }
+
+        private void btnLoadPic_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog op = new OpenFileDialog() { Filter = "JPEG|*.jpg" })
+            {
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    picResult.Image = Image.FromFile(op.FileName);
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cmbModels.Text != "")
+            {
+                if (lstData.FindString(cmbModels.Text, -1) == -1)
+                {
+                    lstData.Items.Add(cmbModels.Text);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstData.Items.Clear();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            saveVOCName();
+            executeYOLO();
+        }
+
+        private void saveVOCName()
+        {
+            StreamWriter saveFile = new StreamWriter("voc.names");
+            foreach (var item in lstData.Items)
+            {
+                saveFile.WriteLine(item.ToString());
+            }
+            saveFile.Close();
+        }
+
+        private void loadVOCName()
+        {
+            lstData.Items.Clear();
+            string line = "";
+            StreamReader loadFile = new StreamReader(cmbModels.Text);
+            while ((line = loadFile.ReadLine()) != null)
+            {
+                lstData.Items.Add(line);
+            }
+            loadFile.Close();
+        }
+
+        private void executeYOLO()
+        {
+            var configDetect = new ConfigurationDetector();
+            var config = configDetect.Detect();
+            var sw = new Stopwatch();
+            sw.Start();
+
+            using (var yoloWrap = new YoloWrapper(cmbPretained.Text, cmbPretained.Text.Replace(".cfg", "") + ".weights", cmbModels.Text)) // kalau ada error, check library
+                                                                                                                                          // using(var yoloWrap = new YoloWrapper(config)) // kalau ada error, check library
+                                                                                                                                          // using(var yoloWrap = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names")) // kalau ada error, check library
+            {
+                using (MemoryStream mem = new MemoryStream())
+                {
+                    picResult.Image.Save(mem, ImageFormat.Png);
+                    var items = yoloWrap.Detect(mem.ToArray());
+                    dgTrackedObject.DataSource = items;
+                    drawRectangle();
+                }
+            }
+
+            sw.Stop();
+            lblStatus.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+            prgStatus.Value = 100;
+        }
+
+        private void populateCmb(ComboBox combo, string fileType)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(Path.GetDirectoryName(Application.ExecutablePath));
+            FileInfo[] fileList = dInfo.GetFiles(fileType);
+            combo.Items.Clear();
+            foreach (FileInfo file in fileList)
+            {
+                combo.Items.Add(file.Name);
+            }
+        }
+
+        // dekat cmbModels, cari selected index change, double click
+        private void cmbModels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            loadVOCName();
+        }
+
+        private void drawRectangle()
+        {
+            var img = picResult.Image;
+
+            using (var canvas = Graphics.FromImage(img))
+            {
+                for (int i = 0; i < dgTrackedObject.Rows.Count; i++)
+                {
+                    var type = dgTrackedObject.Rows[i].Cells[0].Value;
+                    var conf = dgTrackedObject.Rows[i].Cells[1].Value;
+                    var x = dgTrackedObject.Rows[i].Cells[2].Value;
+                    var y = dgTrackedObject.Rows[i].Cells[3].Value;
+                    var width = dgTrackedObject.Rows[i].Cells[4].Value;
+                    var height = dgTrackedObject.Rows[i].Cells[5].Value;
+
+                    using (var oy = new SolidBrush(Color.FromArgb(150, 255, 255, 102)))
+                    using (var pen = getBrush(Convert.ToDouble(conf), Convert.ToInt32(width)))
+                    {
+                        Rectangle rect;
+                        rect = new Rectangle(Convert.ToInt32(x), Convert.ToInt32(y), Convert.ToInt32(width), Convert.ToInt32(height));
+                        canvas.DrawRectangle(pen, rect);
+                    }
+                }
+            }
+            picResult.Image = img;
+        }
+
+        private Pen getBrush(double conf, int width)
+        {
+            var size = width / 100;
+            if (conf > 0.5)
+            {
+                return new Pen(Brushes.Purple, size);
+            }
+            else if (conf < 0.3)
+            {
+                return new Pen(Brushes.Red, size);
+            }
+
+            return new Pen(Brushes.LightGreen, size);
+
+        }
+    }
+}
